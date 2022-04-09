dbf9df1807e59937fa96c74452189883722a744f
Abdul Wafi bin Haji Ismail <wafibismail@gmail.com>
2022-04-09 15:29:21 +0800

    Clear app content & app test content

--- tdd-react_jest_enzyme/src/App.test.js
+++ tdd-react_jest_enzyme/src/App.test.js
@@ -1,8 +1,2 @@
 import { getByText, render, screen } from '@testing-library/react';
 import App from './App';
-
-it('renders learn react link', () => {
-  const { getByText} = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
